1eefc6c2ec51fef3b2ad75a77a6961da7627bbd9
Dawid Studzinski <dawid.studzinski@allegro.pl>
2020-12-17 18:20:33 +0100

    fix

--- src/app/components/CounterHooks.js
+++ src/app/components/CounterHooks.js
@@ -1,8 +1,9 @@
 import {useDispatch, useSelector} from "react-redux";
 
+import {getCounterValue, increment, decrement} from "../store/data/counterReducer";
 
-function CounterHooks({selector, increment, decrement}) {
-  const counterValue = useSelector(selector);
+function CounterHooks() {
+  const counterValue = useSelector(getCounterValue);
   const dispatch = useDispatch();
 
   return <div>
